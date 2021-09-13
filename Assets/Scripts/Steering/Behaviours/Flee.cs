622198fa24626c7e198258804e2450844ab2000b
Duy <etn218n@gmail.com>
2021-09-13 12:45:08 +0300

    Implement Wander and Flee

---
+++ Assets/Scripts/Steering/Behaviours/Flee.cs
@@ -0,0 +1,15 @@
+﻿using Kinematics;
+
+namespace Steering
+{
+    public class Flee : SteeringBehaviour
+    {
+        public override SteeringOutput Steer(Kinematic current, Kinematic target, SteeringThreshold threshold)
+        {
+            var fleeDirection      = (current.Position - target.Position).normalized;
+            var linearAcceleration = fleeDirection * threshold.MaxLinearAcceleration;
+
+            return new SteeringOutput(linearAcceleration, null);
+        }
+    }
+}
---
+++ Assets/Scripts/Steering/Behaviours/Wander.cs
@@ -0,0 +1,19 @@
+﻿using Kinematics;
+using UnityEngine;
+
+namespace Steering
+{
+    public class Wander : SteeringBehaviour
+    {
+        [Range(0.01f, 1.0f)]
+        [SerializeField] private float deviation = 0.05f;
+        
+        public override SteeringOutput Steer(Kinematic current, Kinematic target, SteeringThreshold threshold)
+        {
+            var angularAcceleration = threshold.MaxAngularSpeed * Random.Range(-deviation, deviation) * current.Up;
+            var linearAcceleration  = current.Forward * threshold.MaxLinearAcceleration;
+            
+            return new SteeringOutput(linearAcceleration, angularAcceleration);
+        }
+    }
+}
